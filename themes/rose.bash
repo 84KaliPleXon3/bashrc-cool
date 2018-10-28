d19af7f89994fd5485fd5b1370311452ede5d38e
novranfaathir <44427665+novranfaathir@users.noreply.github.com>
2018-10-29 01:49:50 +0700

    Create rose.bash

---
+++ themes/rose.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+RED='\e[1;31m'
+GREEN='\e[1;32m'
+WHITE='\e[1;37m'
+RESET='\e[39m';
+echo -e "${GREEN}           __                       ";
+echo -e "      _   / /|                      ";
+echo -e "     |\\\\  \/_/                      ";
+echo -e "     \_\| / __                      ";
+echo -e "        \/_/__\           ${RED}.-=='/~\  ";
+echo -e "${GREEN} ____,__/__,_____,______)${RED}/   /{~}}} ";
+echo -e "${GREEN} -,------,----,-----,---,${RED}\'-' {{~}} ";
+echo -e "${GREEN}                         )${RED}'-==.\}/  ${WHITE}";
+echo -e " Hello ${GREEN}$(whoami)${WHITE}!";
+echo -e " Now is $(date +"%d-%b-%Y %H:%M")${RESET}";
+echo "";
