104c2ff0ac0309d6882898bcd94f4f20def9eeaf
novranfaathir <44427665+novranfaathir@users.noreply.github.com>
2018-10-29 11:46:23 +0700

    Create bashrc-cool.bash

---
+++ bashrc-cool.bash
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+if [[ ${1} == "--help" ]];then
+	echo "Help menu";
+	exit;
+elif [[ ${1} == "--update" ]];then
+	if [[ -z $(command -v curl) ]];then
+		echo "ERROR: curl not found, please install curl";
+		exit;
+	fi
+	echo "INFO: Updating...";
+	curl -s ""
+	exit;
+elif [[ -z ${1} ]];then
+	echo -ne "";
+else
+	echo "ERROR: Invalid parameter, please using --help";
+	exit;
+fi
+
+if [[ -z $(command -v curl) ]];then
+	echo "ERROR: curl not found, please install curl";
+	exit;
+fi
+
+if [[ -z $(ls -la ~ | grep '[.]bashrc') ]];then
+	echo "ERROR: ~/.bashrc not found";
+	exit;
+fi
+
+echo " BashRC-Cool 1.0 Configuration ";
+echo "";
+echo " Select your themes : ";
+echo " [1]. Rose";
+echo -ne " >> ";read SELECTED;
+
+cd ~
+CURTHEME=$(echo "$(pwd)/bashrc-cool-current.bash");
+
+if [[ -z $(cat ~/.bashrc | grep bashrc-cool-current.bash) ]];then
+	cd ~
+	CURTHEME=$(echo "$(pwd)/bashrc-cool-current.bash");
+	echo "${CURTHEME}" >> ~/.bashrc
+fi
+if [[ ! -f "${CURTHEME}" ]];then
+	touch ${CURTHEME}
+	chmod 777 ${CURTHEME}
+fi
+if [[ ${SELECTED} == "1" ]];then
+	curl -s "https://raw.githubusercontent.com/novranfaathir/bashrc-cool/master/themes/rose.bash" > ${CURTHEME}
+fi
