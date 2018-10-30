2ab0b2d8915ac5d03422fd5cfc924ccc5e39d024
Novran Faathir <44427665+panophan@users.noreply.github.com>
2018-10-30 15:37:40 +0700

    Update update.bash

--- update.bash
+++ update.bash
@@ -1,1 +1,19 @@
+#!/bin/bash
 
+if [[ $(whoami) != "root" ]]
+then
+	echo "ERROR: Please update with user root"
+	exit
+fi
+
+if [[ -d "/tmp/bashrc-cool" ]]
+then
+	rm -rf /tmp/bashrc-cool
+fi
+
+git clone https://github.com/panophan/bashrc-cool.git /tmp/bashrc-cool
+
+cd /tmp/bashrc-cool
+chmod +x setup.bash
+./setup.bash
+echo "DONE: Update success!"
